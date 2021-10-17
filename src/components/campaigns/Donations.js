068dd5d9b7d828dacd3e04007204f7c0fee1f704
Tova <77296521+Tovi10@users.noreply.github.com>
2021-10-17 21:39:41 +0300

    shuli look if you like the time

--- src/components/campaigns/Donations.js
+++ src/components/campaigns/Donations.js
@@ -18,6 +18,7 @@ export default function Donations() {
                                     <Card title={donation.user.name} bordered={false}>
                                         <div>{`תרם ${donation.card.text}`}</div>
                                         <div>{`בתאריך ${moment(new Date((Date)(donation.date))).format('DD/MM/YYYY')}`}</div>
+                                        <div>{`בשעה ${moment(new Date((Date)(donation.date))).format('h:mm:ss a')}`}</div>
                                         <div>{`וקבל ${donation.card.gift && donation.card.gift.name}`}</div>
                                     </Card>
                                 </Col>
