30e6c084dbd3e78e50347160e47f6ee299390b3f
shulamit-leader <74709557+shulamit8562@users.noreply.github.com>
2021-10-26 16:00:27 +0300

    improve firebase

--- src/components/campaigns/Stam.js
+++ src/components/campaigns/Stam.js
@@ -1,38 +1,139 @@
 import React, { useEffect, useState } from 'react';
+import { Form, Alert,Button } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { DeleteTwoTone, UploadOutlined, } from '@ant-design/icons';
+import { firebase } from '../../services/firebase.service';
+import { actions } from '../../redux/actions';
+
 
 export default function Stam() {
 
-
-    const [index, setIndex] = useState(1);
-    const showIndex = (n) => {
-        const x = document.getElementsByClassName("mySlides");
-        let slideIndex = n;
-        if (n > x.length) {
-            slideIndex = 1
-        }
-        if (n < 1) {
-            slideIndex = x.length
-        }
-        for (let i = 0; i < x.length; x[i].style.display = "none", i++);
-        x[slideIndex - 1].style.display = "block";
-        setIndex(slideIndex)
-    }
+    const dispatch = useDispatch();
+    const campaign = useSelector(state => state.campaignReducer.campaign)
+    const [imagesFiles, setImagesFiles] = useState([]);
+    const [imagesURLs, setImagesURLs] = useState([]);
+    const [uploadErr, setUploadErr] = useState(false);
+    const [form] = Form.useForm();
 
     useEffect(() => {
-        showIndex(1)
+        if (!campaign) {
+            dispatch(actions.getCampaignById('614632dd070df86687d7f792'))
+          
+        }
     }, [])
+
+    useEffect(()=>{
+        if(campaign&&campaign.images.length){
+              // form.setFieldsValue(campaign)
+              setImagesURLs(campaign.images)
+        }
+    },[campaign])
+
+    const uploadImage = (event) => {
+        setUploadErr(false);
+        let imgs = [...imagesURLs], files = [...imagesFiles], index = imagesURLs.length;
+        for (const [key, value] of Object.entries(event.target.files)) {
+            if (index === 5) {
+                setUploadErr(true);
+                break;
+            }
+            index++;
+            // display images in <img/>
+            imgs.push(URL.createObjectURL(value));
+            files.push(value);
+        }
+        setImagesURLs(imgs);
+        setImagesFiles(files)
+    }
+
+    const removeImage = (key) => {
+        setImagesFiles(imagesFiles.filter(file => file.name !== imagesFiles[key].name))
+        setImagesURLs(imagesURLs.filter(img => img !== imagesURLs[key]))
+    }
+
+    const removeAll = () => {
+        setImagesFiles([]);
+        setImagesURLs([]);
+    }
+
+    
+    const uploadImageToStorage = () => {
+        let images = [], imagePaths = [];
+        if (!imagesURLs) {
+            // maybe move them to the end of the function;
+            return;
+        };
+      
+        const storageRef = firebase.storage().ref();
+        imagesFiles.map(async img => {
+            let fileRef = storageRef.child(`Campaigns/614632dd070df86687d7f792/${img.name}`);
+            await fileRef.put(img);
+            const singleImgPath = await fileRef.getDownloadURL();
+            imagePaths.push(singleImgPath);
+            // the end :)
+            if (imagePaths.length === images.length) {
+                const updateCampaign = { ...campaign, images: imagePaths, create: true }
+                debugger
+                // dispatch(actions.updateCampaign(updateCampaign))
+            }
+        });
+    }
+
+    const onFinish = (values) => {
+        console.log("🚀 ~ file: Stam.js ~ line 55 ~ onFinish ~ values", values)
+        console.log("🚀 ~ file: Stam.js ~ line 55 ~ onFinish ~ imagesURLs", imagesURLs)        
+        console.log("🚀 ~ file: Stam.js ~ line 55 ~ onFinish ~ imagesFiles", imagesFiles)      
+        uploadImageToStorage();  
+    }
+
 
     return (
         <div className='Stam'>
-            <div className="w3-content w3-display-container">
-                <img alt='img1' className="mySlides" src='ljogo192.png' style={{ background: 'red' }} />
-                <img alt='img2' className="mySlides" src="img_lights.jpg" style={{ background: 'blue' }} />
-                <img alt='img3' className="mySlides" src="img_forest.jpg" style={{ background: 'yellow' }} />
-                <img alt='img4' className="mySlides" src="img_mountains.jpg" />
-
-                <button onClick={e => showIndex(index - 1)}>prev</button>
-                <button onClick={e => showIndex(index + 1)}>next</button>
-            </div>
+            <div>stam</div>
+            <div onClick={removeAll}>removeAll</div>
+            <Form
+                form={form}
+                name="campaign"
+                onFinish={onFinish}>
+                <Form.Item
+                    name="images"
+                    style={{ display: 'inline-block', width: 'calc(100% - 8px)', marginLeft: '8px' }}
+                    rules={[
+                        {
+                            required: uploadErr,
+                            message: `העלה מקסימום 5 תמונות!`,
+                        },
+                    ]}
+                >
+                    {/* limit num og images to 5 */}
+                    {imagesFiles.length < 5 &&
+                        <div className='btn mb-2 d-flex justify-content-center uploadLogoDiv'>
+                            <input type='file' accept='image/*' multiple={true}
+                                onChange={e => uploadImage(e)} className='uploadHiddenInput' />
+                            <UploadOutlined className='plusIcon' />
+                            <div>בחר תמונות לקמפיין</div>
+                        </div>}
+                    <div className='d-flex align-items-center justify-content-around'>
+                        {imagesURLs.length ? imagesURLs.map((i, key) => (
+                            <div key={key} className='wrapperImgs' >
+                                <DeleteTwoTone twoToneColor="#5ddf5d" className='deleteImgIcon' title={`מחק תמונה`} onClick={() => removeImage(key)} />
+                                <img alt='img' src={i} style={{ width: '100%', height: '20vh', objectFit: 'contain' }} />
+                            </div>
+                        )) : ''}
+                    </div>
+                    {uploadErr && <Alert
+                        style={{ width: 'calc(100% - 8px)', margin: '10px 0 0 8px' }}
+                        message="העלה מקסימום 5 תמונות!" type="error" closable />}
+                </Form.Item>
+                {/* submit */}
+                <Form.Item
+                    style={{ display: 'inline-block', width: 'calc(100% - 8px)', marginLeft: '8px' }}
+                    className='submitFormItem'>
+                    <Button size='large' type="primary" htmlType="submit" className='btnSubmit'>
+                        SAVE                    </Button>
+                </Form.Item>
+            </Form>
         </div>
     )
 }
--- src/components/management/CreateCampaign.js
+++ src/components/management/CreateCampaign.js
@@ -32,15 +32,15 @@ function CreateCampaign(props) {
 
     const [form] = Form.useForm();
     const [logo, setLogo] = useState(null);
-    const [imageList, setImageList] = useState(null);
     const [createCompany, setCreateCompany] = useState(false);
     const [spining, setSpining] = useState(false);
     const [uploadErr, setUploadErr] = useState(false);
     const [logoURL, setLogoURL] = useState(null);
-    const [imagesURL, setImagesURL] = useState([]);
     const [openModal, setOpenModal] = useState(false);
+    const [imagesFiles, setImagesFiles] = useState([]);
+    const [imagesURLs, setImagesURLs] = useState([]);
 
-    const inputLogoRef = useRef(null), inputImagesRef = useRef(null);
+    const inputLogoRef = useRef(null);
 
     useEffect(() => {
         dispatch(actions.getAllGifts());
@@ -59,32 +59,31 @@ function CreateCampaign(props) {
         }
     }, [general.companyId])
 
-    const addImages = (event) => {
-        const files = inputImagesRef.current.files;
-        if (files.length > 5) {
-            setUploadErr(true);
-        }
-        else {
-            setUploadErr(false);
-            // display images in <img/>
-            let imgs = [];
-            for (const [key, value] of Object.entries(event.target.files)) {
-                imgs.push(URL.createObjectURL(value));
+    const uploadImage = (event) => {
+        setUploadErr(false);
+        let imgs = [...imagesURLs], files = [...imagesFiles], index = imagesURLs.length;
+        for (const [key, value] of Object.entries(event.target.files)) {
+            if (index === 5) {
+                setUploadErr(true);
+                break;
             }
-            setImagesURL(imgs);
-            // NOW, overwrites the photos with another upload
-            // if (imageList)
-            //     setImageList(files, ...imageList)
-            // else
-            setImageList(files)
+            index++;
+            // display images in <img/>
+            imgs.push(URL.createObjectURL(value));
+            files.push(value);
         }
+        setImagesURLs(imgs);
+        setImagesFiles(files)
     }
 
     const removeImage = (key) => {
-        const files = Object.entries(imageList).filter(file => file[0] !== key);
-        setImageList(Object.fromEntries(files));
-        // remove also from imagesURL
-        setImagesURL(imagesURL.filter(img => img !== imagesURL[key]));
+        setImagesFiles(imagesFiles.filter(file => file.name !== imagesFiles[key].name))
+        setImagesURLs(imagesURLs.filter(img => img !== imagesURLs[key]))
+    }
+
+    const removeAll = () => {
+        setImagesFiles([]);
+        setImagesURLs([]);
     }
 
     const onFinish = (values) => {
@@ -100,24 +99,21 @@ function CreateCampaign(props) {
     }
 
     const uploadImageToStorage = (campaignId) => {
-        let images = [], imagePaths = [];
-        if (!imageList) {
+        let imagePaths = [];
+        if (!imagesURLs) {
             // maybe move them to the end of the function;
             dispatch(actions.setCampaignId(null));
             setOpenModal(true);
             return;
         };
-        for (const [key, value] of Object.entries(imageList)) {
-            images.push(value);
-        }
         const storageRef = firebase.storage().ref();
-        images.map(async img => {
+        imagesFiles.map(async img => {
             let fileRef = storageRef.child(`Campaigns/${campaignId}/${img.name}`);
             await fileRef.put(img);
             const singleImgPath = await fileRef.getDownloadURL();
             imagePaths.push(singleImgPath);
             // the end :)
-            if (imagePaths.length === images.length) {
+            if (imagePaths.length === imagesFiles.length) {
                 const updateCampaign = { ...campaign, images: imagePaths, create: true }
                 dispatch(actions.updateCampaign(updateCampaign))
                 dispatch(actions.setCampaignId(null));
@@ -362,28 +358,30 @@ function CreateCampaign(props) {
                                 <Form.Item
                                     name="images"
                                     style={{ display: 'inline-block', width: 'calc(100% - 8px)', marginLeft: '8px' }}
-                                // rules={[
-                                //     {
-                                //         required: uploadErr,
-                                //         message: `העלה מקסימום 5 תמונות!`,
-                                //     },
-                                // ]} 
+                                    // rules={[
+                                    //     {
+                                    //         required: uploadErr,
+                                    //         message: `העלה מקסימום 5 תמונות!`,
+                                    //     },
+                                    // ]}
                                 >
                                     {/* limit num og images to 5 */}
-                                    {(!imageList || imageList.length < 5 || !imageList.length) &&
+                                    {imagesFiles.length < 5 &&
                                         <div className='btn mb-2 d-flex justify-content-center uploadLogoDiv'>
-                                            <input type='file' accept='image/*' multiple={true} ref={inputImagesRef} onChange={event => addImages(event)} className='uploadHiddenInput' />
+                                            <input type='file' accept='image/*' multiple={true}
+                                                onChange={e => uploadImage(e)} className='uploadHiddenInput' />
                                             <UploadOutlined className='plusIcon' />
                                             <div>בחר תמונות לקמפיין</div>
                                         </div>}
-                                    {imageList && <div className='d-flex align-items-center justify-content-around'>
-                                        {Object.entries(imageList).map(([key, value], i) => (
+                                    <div className='d-flex align-items-center justify-content-around'>
+                                        {imagesURLs.length ? imagesURLs.map((i, key) => (
                                             <div key={key} className='wrapperImgs' >
                                                 <DeleteTwoTone twoToneColor="#5ddf5d" className='deleteImgIcon' title={`מחק תמונה`} onClick={() => removeImage(key)} />
-                                                <img alt='img' src={imagesURL[i]} style={{ width: '100%', height: '20vh', objectFit: 'contain' }} />
+                                                <img alt='img' src={i} style={{ width: '100%', height: '20vh', objectFit: 'contain' }} />
                                             </div>
-                                        ))}
-                                    </div>}
+                                        )) : ''}
+                                    </div>
+                                    <div onClick={removeAll}>removeAll</div>
                                     {uploadErr && <Alert
                                         style={{ width: 'calc(100% - 8px)', margin: '10px 0 0 8px' }}
                                         message="העלה מקסימום 5 תמונות!" type="error" closable />}
