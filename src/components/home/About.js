5731f797ef688816cbf446e625f9e53a9ea3c49e
Tova <77296521+Tovi10@users.noreply.github.com>
2021-11-22 19:34:32 +0200

    finish about

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -1,72 +1,38 @@
 import React from 'react';
 import ApplyCreateCampaign from '../general/CreateApply';
-// import "animate.css"
-// import Board from '../campaigns/Note';
-// import Note from '../campaigns/try';
+
+import ourAcitvityImg from '../../assets/ourAcitvity.png';
 
 export default function About() {
 
     return (
         <div className='About'>
- <p className="homeText">
-                unsatiable it on moment up elsewhere
-                <br />ecstatic easily near half mrs On garden Ham in humoured mean mrs found him
-                <br />plan indeed Likewise speaking shortly wished forming had all
-                <br />mile she weeks law few on continual be deal On Home part to get right
-                <br />Dear it an Total questions how mr yesterday money these at temper are have
-                <br />horrible he get besides too feeling nay not Polite of she her
-                <br />Not are put on as had wholly or Visited compliment those in yet ye or curiosity songs
-                <br />where wondered Melancholy trifling So melancholy seems remaining
+            <img className="ourAcitvityImg" src={ourAcitvityImg} />
+            <p className="aboutText">
+                כל יו&quot;ר ארגון חסד או עמותת צדקה
+                <br />יודע שרק בזכות התורמים
+                <br />הוא מצליח להניף את הפעילות החשובה שלו
+                <br />בלעדיהם, הוא לא היה מחזיק מעמד
+                <br />&quot;גיפט מאצ&quot;
+                <br />להביע על את הערכתם על תרומתכם החשובה.
+                <br />בשיטה המיוחדת שבה עובד האתר
+                <br />אתם מוזמנים לבחור
+                <br />את גודל הסכום ואת השי הנחשק
+                <br />והשובר ישלח ישירות למייל
+                <br /><b>ביחד נגיע ליעד!</b>
+                <br />
+                <br />
+                <br />אז אם אתם התורמים - כנסו לארגון שאתם מוקירים ועל כל תרומה תקבלו מתנה,
+                <br /><b>אם אתם ראשי עמותה - מוזמנים לפנות אלי</b>
+                <br /><b>ולבחור את המסלול המתאים לכם!</b>
+                <br />
+                <br />אני כאן לכל שאלה,
+                <br />אושרת ישראלי
             </p>
-            {/* <div className="aboutText">
-                <p>
-                    מחפש קריאה מהירה באינטרנט? להלן תמצא את האתרים האהובים עלינו לקרוא עליהם סיפורים קצרים.
-                    אם אתה קורא את הפוסט הזה, רוב הסיכויים שגלשת דרך פייסבוק, טוויטר, Tumblr או Reddit במשך כחצי השעה האחרונה. סביר להניח שתרצה לעשות משהו פרודוקטיבי יותר עם הזמן שלך - תקרא ספר, למשל - אבל פשוט אין לך מספיק זמן בידיים כדי להתחייב לרומן בעל משקל של 400 עמודים.
-                    מקובל כרגע:
-                    ▸ דברים שכדאי לדעת לפני שאתה קונה Kindle Paperwhite 6.8 (דגם 2021)
-                    - 26 בספטמבר 2021
-                    ▸ הספרים הטובים ביותר ללא הגבלת קינדל לקריאה בשנת 2021 - לפי קטגוריה
-                    - 5 בספטמבר 2021
-                    אבל מה אם יש דרך אחרת?
-                    התשובה פשוטה: סיפורים קצרים. סיפורים קצרים שיכולים להתמכר, לשקוע ולספוג בדיוק כמו שרומן יכול - אך בזמן של נסיעה אחת בבוקר או אמבטיה מרגיעה.
-                    ויש הרבה מקומות למצוא קריאות מהירות באיכות גבוהה באינטרנט, לעתים קרובות בחינם. בחלק מהאתרים יש אפילו אפליקציות iOS לקריאה, מה שהופך את חווית הקריאה לקלה עוד יותר.
-                    גלול למטה כדי לגלות אילו אתרי סיפורים קצרים עשו את החיתוך.
-                    מדוע סיפורים קצרים הם קריאה מצוינת
-                    היות שאין מספיק זמן להתחייב לקריאת ספר כבד משקל היא אחת הסיבות העיקריות לכך שכל כך הרבה אנשים נופלים
-                    התשובה פשוטה: סיפורים קצרים. סיפורים קצרים שיכולים להתמכר, לשקוע ולספוג בדיוק כמו שרומן יכול - אך בזמן של נסיעה אחת בבוקר או אמבטיה מרגיעה.
-                    ויש הרבה מקומות למצוא קריאות מהירות באיכות גבוהה באינטרנט, לעתים קרובות בחינם. בחלק מהאתרים יש אפילו אפליקציות iOS לקריאה, מה שהופך את חווית הקריאה לקלה עוד יותר.
-                    גלול למטה כדי לגלות אילו אתרי סיפורים קצרים עשו את החיתוך.
-                    מדוע סיפורים קצרים הם קריאה מצוינת
-                    היות שאין מספיק זמן להתחייב לקריאת ספר כבד משקל היא אחת הסיבות העיקריות לכך שכל כך הרבה אנשים נופלים
-                </p>
-            </div> */}
+            {/* <
             <div>
                 {/* <Board /> */}
-                {/* <Note /> */}
-            </div>
-            {/* https://animate.style/ */}
-            {/* <h1 className="animate__animated animate__flipOutY">An animated element</h1> */}
-            {/* <div className="bg"></div>
-            <div className="bg bg2"></div>
-            <div className="bg bg3"></div> */}
-            {/* <div className="content">
-                <div id="bars1">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </div> */}
-            {/* <div className="wrapper" >
-                <div className="typing-demo" >
-                    זה המקום לספר על האתר שלנו כמה הוא יפה ומשוכלל!!
-                </div >
-            </div> */}
-            {/* </div> */}
-            {/* </div >
-                </div> */}
-            {/* </div> */}
+            {/* <Note /> */}
             <ApplyCreateCampaign />
         </div >
     )
